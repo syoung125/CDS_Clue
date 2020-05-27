313cd7963cd836cfa70a1eccae1421e6db924f65
syoung125 <laura120555@gmail.com>
2020-05-27 20:04:29 +0900

    다시 시작

--- CDS_Clue/src/test.java
+++ CDS_Clue/src/test.java
@@ -2,11 +2,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("pull 성공!gywlsㅎㅎ");
-		System.out.println("pull 성공!승연ㅎㅎ");
-		System.out.println("pull 성공!서영ㅎㅎ");
-		System.out.println("pull 성공!효진ㅎㅎ");
-		System.out.println("pull 성공!효진2ㅎㅎ");
+		System.out.println("pull 침착해");
 
 	}
 
