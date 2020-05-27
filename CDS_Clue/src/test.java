014a98e552c4cd82d2d36ac5e8e0e9dc373be9fe
dhffkvm15 <dhffkvm15@gmail.com>
2020-05-27 20:12:04 +0900

    dhffkvm15 pull

--- CDS_Clue/src/test.java
+++ CDS_Clue/src/test.java
@@ -3,7 +3,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("pull gywls");
-		System.out.println("pull 뜽연");
+		System.out.println("pull �쑞�뿰");
+		System.out.println("pull dhffkvm15");
 	}
 
 }
