5a12558255da9160d551214de4d88abb8964a4e6
dhffkvm15 <dhffkvm15@gmail.com>
2020-05-27 20:18:35 +0900

    pull

--- CDS_Clue/src/test.java
+++ CDS_Clue/src/test.java
@@ -6,10 +6,8 @@ public class test {
 <<<<<<< HEAD
 		System.out.println("pull �쑞�뿰");
 		System.out.println("pull dhffkvm15");
-=======
 		System.out.println("pull 뜽연");
 		System.out.println("pull 뜽연2");
->>>>>>> branch 'master' of https://github.com/syoung125/CDS_Clue.git
 	}
 
 }
