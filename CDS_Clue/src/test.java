e43438d834b5b6d6b303c9d026e0c2f60165c641
syoung125 <laura120555@gmail.com>
2020-05-27 19:52:23 +0900

    서영 test

--- CDS_Clue/src/test.java
+++ CDS_Clue/src/test.java
@@ -4,6 +4,7 @@ public class test {
 		// TODO Auto-generated method stub
 		System.out.println("pull 성공!gywlsㅎㅎ");
 		System.out.println("pull 성공!승연ㅎㅎ");
+		System.out.println("pull 성공!서영ㅎㅎ");
 	}
 
 }
