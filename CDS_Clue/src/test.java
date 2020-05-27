4c7d570670846e1b454c6616d73ca3fd0262010a
Gywlssww <tarahjjeon96@gmail.com>
2020-05-27 19:52:03 +0900

    "효진"

--- CDS_Clue/src/test.java
+++ CDS_Clue/src/test.java
@@ -4,6 +4,8 @@ public class test {
 		// TODO Auto-generated method stub
 		System.out.println("pull 성공!gywlsㅎㅎ");
 		System.out.println("pull 성공!승연ㅎㅎ");
+		System.out.println("pull 성공!효진ㅎㅎ");
+
 	}
 
 }
