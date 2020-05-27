246e075f9cb133ea3a42ba1b9b9e39c532fddd9d
seungyeonchoi <bzt2971@gmail.com>
2020-05-27 19:50:41 +0900

    seungyeon

--- CDS_Clue/src/test.java
+++ CDS_Clue/src/test.java
@@ -1,9 +1,9 @@
-
 public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("pull 성공!gywlsㅎㅎ");
+		System.out.println("pull 성공!승연ㅎㅎ");
 	}
 
 }
