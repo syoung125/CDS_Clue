22dffe7b5fc104ff2e7a0a96358ce3b5e818f16a
dhffkvm15 <dhffkvm15@gmail.com>
2020-06-03 15:11:09 +0900

    mozi

--- CDS_Clue/src/test.java
+++ CDS_Clue/src/test.java
@@ -2,7 +2,7 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
+
 		System.out.println("pull gywls");
 
 		System.out.println("pull 깨졌던거");
@@ -10,8 +10,7 @@ public class test {
 
 		System.out.println("pull 뜽연");
 		System.out.println("pull 뜽연2");
-=======
->>>>>>> branch 'master' of https://github.com/syoung125/CDS_Clue.git
+
 	}
 
 }
