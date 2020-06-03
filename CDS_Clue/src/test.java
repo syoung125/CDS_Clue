ba8c60ee2ee3e3def03a98c70e355ea3f4c8cc4c
syoung125 <laura120555@gmail.com>
2020-06-03 14:18:16 +0900

    empty

--- CDS_Clue/src/test.java
+++ CDS_Clue/src/test.java
@@ -2,10 +2,6 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("pull gywls");
-		System.out.println("pull dhffkvm15");
-		System.out.println("pull 뜽연");
-		System.out.println("pull 뜽연2");
 	}
 
 }
