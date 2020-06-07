e90e5e9fc2ec163ae2eb0c7a8b6c2374c78b9b3f
Gywlssww <tarahjjeon96@gmail.com>
2020-06-07 17:08:24 +0900

    클루서버

--- CDS_Clue/src/clue/ClueServerEventHandler.java
+++ CDS_Clue/src/clue/ClueServerEventHandler.java
@@ -3,6 +3,7 @@ package clue;
 
 import java.util.Iterator;
 
+import kr.ac.konkuk.ccslab.cm.entity.CMGroupInfo;
 import kr.ac.konkuk.ccslab.cm.entity.CMSessionInfo;
 import kr.ac.konkuk.ccslab.cm.event.CMDummyEvent;
 import kr.ac.konkuk.ccslab.cm.event.CMEvent;
@@ -24,13 +25,26 @@ public class ClueServerEventHandler implements CMAppEventHandler{
 	public void processEvent(CMEvent arg0) {
 		// TODO Auto-generated method stub
 		switch(arg0.getType()) {
-		
+		case CMInfo.CM_SESSION_EVENT:
+			processSessionEvent(arg0);
+			break;
+		default:
+			return;
 			
 			
 		}
    
 	}
-
+	private void processSessionEvent(CMEvent arg0) {
+		
+		CMSessionEvent se=(CMSessionEvent) arg0;
+		Iterator<CMGroupInfo> iter=se.getGroupInfoList().iterator();
+        while(iter.hasNext()) {
+			CMGroupInfo gInfo=iter.next();
+			System.out.println(gInfo.getGroupName());
+			
+		}
+	}
 	public CMServerStub getM_serverStub() {
 		return m_serverStub;
 	}
