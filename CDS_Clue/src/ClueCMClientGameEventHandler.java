387cc99261b47538329ba6ec1d8b30fdffa46332
syoung125 <laura120555@gmail.com>
2020-06-13 11:34:41 +0900

    1. 버튼색 변경 2. init게임과 연동하는 함수 생성

--- CDS_Clue/src/ClueCMClientGameEventHandler.java
+++ CDS_Clue/src/ClueCMClientGameEventHandler.java
@@ -3,12 +3,17 @@ import kr.ac.konkuk.ccslab.cm.event.CMEvent;
 import kr.ac.konkuk.ccslab.cm.event.handler.CMAppEventHandler;
 import kr.ac.konkuk.ccslab.cm.info.CMInfo;
 import kr.ac.konkuk.ccslab.cm.stub.CMClientStub;
-
+import org.json.*;
 public class ClueCMClientGameEventHandler implements CMAppEventHandler {
 	
 	private ClueCMClientGame m_client;
 	private CMClientStub m_clientStub;
 	
+	// Dummy info type
+	public class DummyType { 
+		public static final String GameStart = "GameStart";
+	
+	}
 	
 	public ClueCMClientGameEventHandler(CMClientStub clientStub, ClueCMClientGame client)
 	{
@@ -57,8 +62,14 @@ public class ClueCMClientGameEventHandler implements CMAppEventHandler {
 	private void processDummyEvent(CMEvent cme)
 	{
 		CMDummyEvent due = (CMDummyEvent) cme;
-		printMessage("session("+due.getHandlerSession()+"), group("+due.getHandlerGroup()+")\n");
-		printMessage("dummy msg: "+due.getDummyInfo()+"\n");
+//		printMessage("session("+due.getHandlerSession()+"), group("+due.getHandlerGroup()+")\n");
+//		printMessage("dummy msg: "+due.getDummyInfo()+"\n");
+		//parse해서 (type#msg) 이런식으로 전송
+		if(due.getDummyInfo() == DummyType.GameStart) {
+			// 서버에서 받은 것일 때 (할수있나)(왜냐면 채팅에서 GameStart칠수도있으니까)
+			// msg parse -> 인자로 게임초기화 정보 보냄
+//			m_client.setGameStart();
+		}
 		return;
 	}
 	
